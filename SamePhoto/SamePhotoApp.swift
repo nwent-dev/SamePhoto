647f67c4bfb3c29e5f7d473acf9924e9e1af8dd6
alk <nwent@proton.me>
2025-04-10 10:35:03 +0300

    Initial Commit

---
+++ SamePhoto/SamePhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamePhotoApp.swift
+//  SamePhoto
+//
+//  Created by Александр Калашников on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SamePhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
